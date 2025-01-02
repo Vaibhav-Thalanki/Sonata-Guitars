42cdad2c2fc6178d4533fdeb769ce60ba8502a97
Vaibhav-Thalanki <Vaibhav.thalanki21@gmail.com>
2025-01-02 11:23:26 -0500

    minor

--- server/server.js
+++ server/server.js
@@ -40,17 +40,6 @@ app.use((err, req, res, next) => {
 
 app.get("/", (req, res) => res.send("Express on Vercel"))
 
-if (process.env.NODE_ENV === "production") {
-
-    app.use(express.static("client/build"));
-
-    app.get("*", (req, res) => {
-
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-
-   });
-
-}
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
