d755898199d80e834ba175d3c9dec9aa31384917
Vaibhav-Thalanki <Vaibhav.thalanki21@gmail.com>
2025-01-02 23:28:46 -0500

    minor

--- server/routes/transaction.route.js
+++ server/routes/transaction.route.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const auth = require('../middleware/auth')
 const transactionController = require('../controllers/transaction.controller')
 
-router.route('/').post(auth(),transactionController.addTransaction)
+router.route('/').post(transactionController.addTransaction)
 
 module.exports = router
